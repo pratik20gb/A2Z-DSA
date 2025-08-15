bf95e6ca140abe8db1883cca7fb36c3c43278dfa
shori <pratik20gb@gmail.com>
2025-08-15 17:07:48 +0530

    day-01

---
+++ c++.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    // long long float double int 
+    // string getline
+
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<s1<<s2;
+    return 0;
+}
