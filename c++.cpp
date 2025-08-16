b28f5e023512b46d6b0182ef57e13fe8ca7e0aa5
shori <pratik20gb@gmail.com>
2025-08-16 15:31:19 +0530

    working witha completed 50% revision of c++ basics

--- c++.cpp
+++ c++.cpp
@@ -2,12 +2,55 @@
 using namespace std;
 
 int main()
-{   
-    // long long float double int 
+{
+    // long long float double int
     // string getline
+    // // string
+    // string s1,s2;
+    // cin>>s1>>s2;
+    // cout<<s1<<s2;
 
-    string s1,s2;
-    cin>>s1>>s2;
-    cout<<s1<<s2;
+    // // getline
+    // string a;
+    // getline(cin, a);
+    // cout << a;
+
+    // char - takes less space compare to string and is used to declare single characters with single quotes
+    //  char ch = 'P';
+    //  cout<<ch;
+
+    // switch
+
+    // int day;
+    // cin >> day;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout << "Monday";
+    //     break;
+    // case 2:
+
+    //     cout << "Tuesday";
+    //     break;
+    // case 3:
+
+    //     cout << "Wednesday";
+    //     break;
+    // default:
+    //     cout << "Invalid check";
+    //     break;
+    // }
+
+    // arrays and 2d arrays
+
+    // int arr[2];
+    // cin>>arr[0]>>arr[1];
+    // arr[1]+=3;
+    // cout<<arr[1];
+
+    int arr[3][5];
+    arr[1][2] = 21;
+    cout << arr[1][2];
+
     return 0;
 }
